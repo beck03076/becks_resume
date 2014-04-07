9640ab65edd3a71cd033a0948d04b72ae578306c
senthil <think@think.(none)>
2014-04-08 01:53:03 +0530

    omniauth id,key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "853539993661-t6l5ivb9v74uddm0kaq8edvr7jilrr3b.apps.googleusercontent.com", "mLk0to63wB_1Nt5RNRQ2MoRW", {
+  provider :google_oauth2, "412836660909.apps.googleusercontent.com", "9THYqMEcuFRQtuCObnn148cQ", {
     access_type: 'offline',
     scope: 'https://www.googleapis.com/auth/userinfo.email',
     redirect_uri: 'http://www.becksresume.com/auth/google_oauth2/callback'
